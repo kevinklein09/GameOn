402b02db1d1505e8a9f4e8ac2ea05d05ee9aa0b7
rysmith504 <rysmith504@gmail.com>
2022-07-15 12:46:18 -0500

    installed dependencies and added start scripts, app is rendering"

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GameOn
+
+run npm install
+
+after completion, run " npm run build "
+
+open separate terminal run " npm run start "
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const port = 4000;
+const distPath = path.resolve(__dirname, '..', 'dist');
+const app = express();
+
+// Middleware - every request runs thru this middleware
+
+app.use(express.json()); // Parse the request body
+app.use(express.urlencoded({ extended: true })); // Parses url
+app.use(express.static(distPath)); // Statically serve up client directory
+
+
+
+
+/** Place all code above here */
+app.listen(port, () => {
+  console.log(`
+  Listening at://127.0.0.1:${port}
+  `);
+});
