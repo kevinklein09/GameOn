004cb4e6914f83d806d993e225ca2a8fa9e741bf
Royce <royce.taylor789@gmail.com>
2022-07-16 14:41:53 -0500

    jsx wrappers in app.jsx

--- DB/index.js
+++ DB/index.js
@@ -6,7 +6,7 @@ const ENV = require('../.env');
 
 const mongoPass = ENV.MONGODB_PASS;
 
-const DB_URI = `mongodb+srv://root:${mongoPass}@gameon.p78bo.mongodb.net/?retryWrites=true&w=majority`;
+const DB_URI = `mongodb+srv://royce:${mongoPass}@gameon.p78bo.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(DB_URI)
   .then(() => console.log('connected'))
   .catch((err) => console.error(err));
