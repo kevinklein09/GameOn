badd2c4ca37b59a9db8fe8f9f2a03586e3ea7ba5
Bethany Jones <betpetjones@gmail.com>
2022-07-16 09:39:13 -0500

    eslint errors

--- DB/index.js
+++ DB/index.js
@@ -1,9 +1,7 @@
-/* eslint-disable linebreak-style */
 const mongoose = require('mongoose');
 const ENV = require('../.env');
 
 const mongoPass = ENV.MONGODB_PASS;
-console.log(mongoPass);
 
 const DB_URI = `mongodb+srv://root:${mongoPass}@gameon.p78bo.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(DB_URI)
