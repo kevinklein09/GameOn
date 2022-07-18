f6c21c60df74f5815c3a6af3b32a60f59021b42e
rysmith504 <rysmith504@gmail.com>
2022-07-18 11:56:58 -0500

    removed session  creation from wrong index.js

--- DB/index.js
+++ DB/index.js
@@ -6,20 +6,12 @@ const passport = require("passport");
 const mongoose = require('mongoose');
 const ENV = require('../.env');
 const Models = require('./models');
-const express = require('express')
 const app = express();
 
 const { MONGODB_PASS, USERNAME } = ENV;
 
 const DB_URI = `mongodb+srv://${USERNAME}:${MONGODB_PASS}@gameon.p78bo.mongodb.net/?retryWrites=true&w=majority`;
 
-app.use(session({
-  secret: "Our little secret.",
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 mongoose.connect(DB_URI)
   .then(() => console.log('connected'))
