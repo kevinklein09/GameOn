39d1ad831bf8c1132b16fc3c74f8c145f0d574d6
Vincent Tong <vincent.khang.tong@gmail.com>
2022-07-18 14:44:08 -0500

    most input handlnig done for createEvent component

--- client/components/CreateEvent.jsx
+++ client/components/CreateEvent.jsx
@@ -7,6 +7,10 @@ import SportsSelect from './SportsSelect.jsx';
 const CreateEvents = () => {
   const [sport, setSport] = useState('');
   const [description, setDescription] = useState('');
+  const [location, setLocation] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [playerLimit, setPlayerLimit] = useState(0);
 
   const handleSelectSport = (e) => {
     setSport(e.target.value);
@@ -14,6 +18,22 @@ const CreateEvents = () => {
 
   const handleDescription = (e) => {
     setDescription(e.target.value);
+  };
+
+  const handleDate = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleTime = (e) => {
+    setTime(e.target.value);
+  }
+
+  const handleLocation = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const handlePlayerLimit = (e) => {
+    setPlayerLimit(JSON.parse(e.target.value));
   };
 
   return (
@@ -33,17 +53,25 @@ const CreateEvents = () => {
           </textarea>
         </div>
 
+        <div id="playerLimit">
+          <input onChange={(e) => handlePlayerLimit(e)} type='number'></input> # of players
+        </div>
+
         <div id='location'>
-          <input type='text' value='insert location here'></input>
+          <input onChange={(e) => handleLocation(e)} type='text' defaultValue='event address'></input>
         </div>
 
         <div id='equipment'>
-          <input type='text' value='list equipment here'></input>
+          <input type='text' defaultValue='list equipment here'></input>
           <button> add item </button>
         </div>
 
         <div id='date'>
-          <input type='date'></input>
+          <input onChange={(e) => handleDate(e)} type='date'></input>
+        </div>
+
+        <div id='time'>
+          <input onChange={(e) => handleTime(e)} type='time'></input>
         </div>
 
         <div id='submit'>
