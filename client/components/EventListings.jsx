6a854dd5794d20c296194b43a10702ff8c9449fb
Royce <royce.taylor789@gmail.com>
2022-07-19 11:45:09 -0500

    rendered data on client side for events

--- client/components/EventListings.jsx
+++ client/components/EventListings.jsx
@@ -1,33 +1,69 @@
-import React from 'react';
-import Event from './Event.jsx';
+/* eslint-disable no-unused-vars */ 
+/* eslint-disable import/extensions */ 
+/* eslint linebreak-style: ['error', 'windows'] */ 
+/* eslint-disable */
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Sports from './SportsSelect';
+//import Event from './Event.jsx';
 
 const EventListings = () => {
-  const fakeData = [
-    {
-      event: 'basketball',
-      category: 'dribble, dribble',
-    },
-    {
-      event: 'baseball',
-      category: 'home, run',
-    },
-    {
-      event: 'tennis lmfao',
-      category: 'wut',
-    },
-  ];
+  const [events, setEvents] = useState([]);
+
+const getAllEvents = () => {
+  axios.get('/api/eventListings')
+  .then((eventData) => {
+    console.log(eventData);
+    setEvents(eventData.data);
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+}
+useEffect(() => {
+  getAllEvents()
+}, []);
 
   return (
     <div>
       <h1>SEE ALL DA hey whats up</h1>
       <div>
-        {fakeData.map((sport, i) => {
-          return <Event sport={ sport } event={ sport.event } category={ sport.category } key={ `sport ${i}` }/>
-          
-        })}
+       <Sports />
+       <button>Sort by Date</button>
+       <button>Sort by DI ST ANCE</button>
+       {
+        events.map((event, i) => {
+          return (
+            <div class="event" key={ i }>
+              { event.description }
+              <button>goin</button> 
+            </div>
+          )
+        })
+       }
       </div>
     </div>
   );
 };
 
 export default EventListings;
+
+// const fakeData = [
+//   {
+//     event: 'basketball',
+//     category: 'dribble, dribble',
+//   },
+//   {
+//     event: 'baseball',
+//     category: 'home, run',
+//   },
+//   {
+//     event: 'tennis lmfao',
+//     category: 'wut',
+//   },
+// ];
+ {/* {.map((sport, i) => {
+          return <Event sport={ sport } event={ sport.event } category={ sport.category } key={ `sport ${i}` }/>
+          
+        })} */}
