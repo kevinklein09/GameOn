5e3e3d01f7ed1826128e801131eb715743b367b7
Bethany Jones <betpetjones@gmail.com>
2022-07-19 10:49:43 -0500

    axios get users

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Profile = () => (
+const axios = require('axios');
+
+const Profile = () => {
+  useEffect(() => {
+    axios.get('/users')
+      .then((usersData) => {
+        console.log(usersData);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+
+  return (
       <div>
         <h1>
             WELCOME TO THE PROFILE PAGE
         </h1>
+        <p>Hello, User: </p>
       </div>
-);
+  );
+};
 
 export default Profile;
