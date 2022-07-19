94cdc442c8696a27e0c16b570dff80354a97e7b3
Vincent Tong <vincent.khang.tong@gmail.com>
2022-07-18 19:46:11 -0500

    inputs clear after an event is posted

--- client/components/CreateEvent.jsx
+++ client/components/CreateEvent.jsx
@@ -4,23 +4,38 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import SportsSelect from './SportsSelect.jsx';
+import EquipmentList from './EquipmentList.jsx';
+const today = new Date();
 
 const CreateEvents = () => {
   const [sport, setSport] = useState('');
-  const [description, setDescription] = useState('');
-  const [location, setLocation] = useState('');
-  const [date, setDate] = useState('');
-  const [time, setTime] = useState('');
+  const [description, setDescription] = useState('enter description here');
+  const [location, setLocation] = useState('enter event address');
+  const [date, setDate] = useState(`${today.getFullYear()}-${today.getMonth() < 10 ? '0' + (today.getMonth() + 1) : today.getMonth()}-${today.getDate()}`);
+  const [time, setTime] = useState('12:00');
   const [playerLimit, setPlayerLimit] = useState(0);
+  const [equipment, setEquipment] = useState([]);
+  const [item, setItem] = useState('list equipment here');
   let categoryId;
 
-  axios.get('/api/categories')
-    .then((categories) => {
-      categoryId = categories.data.filter((category) => category.category === sport)[0]._id
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  if (sport) {
+    axios.get('/api/categories')
+      .then((categories) => {
+        categoryId = categories.data.filter((category) => category.category === sport)[0]._id
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  const handleEquipmentList = () => {
+    setEquipment([...equipment, item]);
+    setItem('');
+  };
+
+  const handleItem = (e) => {
+    setItem(e.target.value);
+  };
 
   const handleSelectSport = (e) => {
     setSport(e.target.value);
@@ -57,6 +72,15 @@ const CreateEvents = () => {
       players: playerLimit,
       isOpen: true,
     });
+
+    setSport('');
+    setDescription('enter description here');
+    setLocation('enter event address');
+    setPlayerLimit(0);
+    setDate(`${today.getFullYear()}-${today.getMonth() < 10 ? '0' + (today.getMonth() + 1) : today.getMonth()}-${today.getDate()}`);
+    setTime('12:00');
+    setEquipment([]);
+    setItem('list equipment here');
   };
 
   return (
@@ -71,30 +95,31 @@ const CreateEvents = () => {
             rows='5'
             cols='60'
             onChange={(e) => handleDescription(e)}
+            value={description}
           >
-            Enter Description Here
           </textarea>
         </div>
 
         <div id="playerLimit">
-          <input onChange={(e) => handlePlayerLimit(e)} type='number'></input> # of players
+          <input onChange={(e) => handlePlayerLimit(e)} type='number' value={playerLimit}></input> # of players
         </div>
 
         <div id='location'>
-          <input onChange={(e) => handleLocation(e)} type='text' defaultValue='event address'></input>
+          <input onChange={(e) => handleLocation(e)} type='text' value={location}></input>
         </div>
 
         <div id='equipment'>
-          <input type='text' defaultValue='list equipment here'></input>
-          <button> add item </button>
+          <input onChange={(e) => handleItem(e)} type='text' value={item}></input>
+          <button onClick={() => handleEquipmentList()}> add item </button>
         </div>
+        <EquipmentList equipment={equipment}/>
 
         <div id='date'>
-          <input onChange={(e) => handleDate(e)} type='date'></input>
+          <input value={date} onChange={(e) => handleDate(e)} type='date'></input>
         </div>
 
         <div id='time'>
-          <input onChange={(e) => handleTime(e)} type='time'></input>
+          <input value={time} onChange={(e) => handleTime(e)} type='time'></input>
         </div>
 
         <div id='submit'>
---
+++ client/components/EquipmentList.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/extensions */
+/* eslint linebreak-style: ['error', 'windows'] */
+import React from 'react';
+import axios from 'axios';
+
+const EquipmentList = ({ equipment }) => (
+  equipment.map((item, i) => (
+    <li key={i}>{item}</li>
+  ))
+);
+
+export default EquipmentList;
