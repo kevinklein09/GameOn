8d37d5749f5571149eac113b58b73e0a0cba2d4b
Bethany Jones <betpetjones@gmail.com>
2022-07-20 10:38:24 -0500

    add context provider to index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,7 @@
 import { createRoot } from 'react-dom/client';
 import React, { useState, createContext } from 'react';
 import {
-  Routes, Route, HashRouter, BrowserRouter
+  Routes, Route, HashRouter, BrowserRouter,
 } from 'react-router-dom';
 import App from './components/App.jsx';
 import Map from './components/Map.jsx';
@@ -17,13 +17,24 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-const UserContext = createContext();
-
 const root = createRoot(document.getElementById('root'));
 // const [user, setUser] = useState('{username: user, email: user@gmail.com}');
+const UserContext = createContext();
+
+class UserContextProvider extends React.Component {
+  state = {
+    user: { username: 'username', email: 'user@gmail.com' },
+  };
+
+  render() {
+    return (
+    <UserContext.Provider value = {{ ...this.state }} />
+    );
+  }
+}
 
 root.render(
-  <UserContext.Provider value={'{username: user, email: user@gmail.com}'}>
+  <UserContextProvider>
   <BrowserRouter>
     <Routes>
       <Route exact path="/" element= {<App />}>
@@ -39,5 +50,5 @@ root.render(
       </Route>
     </Routes>
   </BrowserRouter>
-  </UserContext.Provider>,
+  </UserContextProvider>,
 );
