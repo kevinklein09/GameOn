cb4413ca39ce0c26be26158238ed78a20304a806
Bethany Jones <betpetjones@gmail.com>
2022-07-19 19:52:00 -0500

    Add description to home component

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,11 +1,60 @@
 import React from 'react';
-
-const Home = () => {
- return (
+import Typography from '@mui/material/Typography';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Link } from 'react-router-dom';
+const Home = () => (
       <div>
-     
+    <Typography variant="h4">Welcome to GameOn.</Typography>
+    <Typography>We're excited that you're here. If you want to find people to play local pick up games, you've come to the right place. With our app you can join scheduled games ranging from football to volleyball to even ping pong! Never again will you miss out on playing your favorite sport with like-minded people. <Link to="/login">Sign up</Link> for an account today to get started!</Typography>
+    <br></br>
+    <Typography variant="h5">FAQs</Typography>
+    <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            We're excited that you're here. If you want to find people to play local pick up games, you've come to the right place. 
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
       </div>
-  );
-
- }
+);
 export default Home;
