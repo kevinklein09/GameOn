4e4363fa4cab1d5b6722d907571391b6be80d777
Vincent Tong <vincent.khang.tong@gmail.com>
2022-07-21 12:41:19 -0500

    profile renders differently if user is logged in

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -8,8 +8,11 @@ import axios from 'axios';
 
 
 const Profile = () => {
-  console.log(user);
   const user = useContext(UserContext);
+  console.log(user);
+
+  if (user) {
+    
   const [userEvents, setUserEvents] = useState([]);
   const getUserEvents = () => {
     axios.get('/api/eventListings')
@@ -57,43 +60,50 @@ const Profile = () => {
     }
   }
 
-  const userOrNull = () => { return user ? user.firstName : 'please login' };
 
-  return (
-    <div>
-      <h1>
-          WELCOME TO THE PROFILE PAGE
-      </h1>
-      <p>Hello, User: </p>
+    return (
       <div>
-        <h2>NAME</h2>
-        <p>{ userOrNull() }</p>
-      </div>
-      <div>
-        <h2>EMAIL</h2>
-        <p>{ userOrNull() }</p>
-      </div>
-      <div>
-        <h2>USERNAME</h2>
-        <input type='text' id='username' name ='username' value='{modifiable username here}'></input>
-      </div>
-      <div>
-        <h2>EVENTS CREATED</h2>
-        {userEvents.map((event, i) =>
-        <div key={i} style={{display: 'flex', backgroundColor: 'white', marginTop: '10px', whiteSpace: 'nowrap'}}>
-          <p style={{marginLeft: '10px'}}>{event.catName}</p>
-          <p style={{marginLeft: '30px'}}><b>Date: </b>{`${event.date}`.substring(0, 10)}</p>
-          <p style={{marginLeft: '30px'}}><b>Location: </b>{event.address}</p>
-          <button onClick={() => handleDelete(event._id)} style={{marginLeft: 'auto'}}> delete </button>
+        <h1>
+            WELCOME TO THE PROFILE PAGE
+        </h1>
+        <p>Hello, User: </p>
+        <div>
+          <h2>NAME</h2>
+          <p>{ user.firstName } { user.lastName }</p>
         </div>
-        )}
+        <div>
+          <h2>EMAIL</h2>
+          <p>{ user.email }</p>
+        </div>
+        <div>
+          <h2>USERNAME</h2>
+          <input type='text' id='username' name ='username' value='{modifiable username here}'></input>
+        </div>
+        <div>
+          <h2>EVENTS CREATED</h2>
+          {userEvents.map((event, i) =>
+          <div key={i} style={{display: 'flex', backgroundColor: 'white', marginTop: '10px', whiteSpace: 'nowrap'}}>
+            <p style={{marginLeft: '10px'}}>{event.catName}</p>
+            <p style={{marginLeft: '30px'}}><b>Date: </b>{`${event.date}`.substring(0, 10)}</p>
+            <p style={{marginLeft: '30px'}}><b>Location: </b>{event.address}</p>
+            <button onClick={() => handleDelete(event._id)} style={{marginLeft: 'auto'}}> delete </button>
+          </div>
+          )}
+        </div>
+        <div>
+          <h2>EVENTS ATTENDING</h2>
+          <input type='text' id='eventAttending' name ='eventsAttending' value='{modifiable events user is attending here}'></input>
+        </div>
       </div>
+    );
+
+  } else {
+    return (
       <div>
-        <h2>EVENTS ATTENDING</h2>
-        <input type='text' id='eventAttending' name ='eventsAttending' value='{modifiable events user is attending here}'></input>
+        <h1>PLEASE LOG IN </h1>
       </div>
-    </div>
-  );
+    )
+  }
 };
 
 export default Profile;
