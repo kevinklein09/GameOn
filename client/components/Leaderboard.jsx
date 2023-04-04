f2eb13d22b3872921a8043595c8e59738c7cb28a
kevin <kklein166@gmail.com>
2023-04-04 17:41:39 -0500

    create getUsers functions and invoke in useEffect for Leaderboard

--- client/components/Leaderboard.jsx
+++ client/components/Leaderboard.jsx
@@ -4,21 +4,36 @@ import { makeStyles } from '@mui/styles';
 import { Typography, Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
 
 const Leaderboard = () => {
-  const [leaderboardData, setLeaderboardData] = useState([]);
+  // const [leaderboardData, setLeaderboardData] = useState([]);
+  const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    axios.get('/api/leaderboard')
-      .then((lbData) => {
-        setLeaderboardData(lbData.data);
+  // useEffect(() => {
+  //   axios.get('/api/leaderboard')
+  //     .then((lbData) => {
+  //       setLeaderboardData(lbData.data);
+  //     })
+  //     .catch((err) => {
+  //       console.error(err);
+  //     });
+  // }, []);
+  const getUsers = () => {
+    axios.get('/users')
+      .then((usersObj) => {
+        console.log(usersObj);
+        setUsers(usersObj.data);
       })
       .catch((err) => {
         console.error(err);
       });
+  };
+
+  useEffect(() => {
+    getUsers();
   }, []);
 
   return (
     <TableContainer component={Paper}>
-      <Typography>LeaderBoard</Typography>
+      <Typography variant="h4" component="h4" align="center">LeaderBoard</Typography>
       <Table aria-label="leaderboard">
         <TableHead>
           <TableRow>
