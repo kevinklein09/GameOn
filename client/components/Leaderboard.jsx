3d3f280ca10d090b064ca4fa530197f49aab921e
kevin <kklein166@gmail.com>
2023-04-04 15:14:09 -0500

    add leaderboard route to index.jsx

--- client/components/Leaderboard.jsx
+++ client/components/Leaderboard.jsx
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyle } from '@mui/material/styles';
+import axios from 'axios';
 
 const Leaderboard = () => {
-  const [data, setData] = useState([]);
+  const [leaderboardData, setLeaderboardData] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/leaderboard')
+      .then((lbData) => {
+        setLeaderboardData(lbData.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  })
 };
 
 export default Leaderboard;
