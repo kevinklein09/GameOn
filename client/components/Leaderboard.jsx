bb92d84a8aecfb5d8efa3ac018df67d8d9dc97a2
kevin <kklein166@gmail.com>
2023-04-04 14:54:10 -0500

    add state to Leaderboard

--- client/components/Leaderboard.jsx
+++ client/components/Leaderboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { makeStyle } from '@mui/material/styles';
 
 const Leaderboard = () => {
-  
+  const [data, setData] = useState([]);
 };
 
 export default Leaderboard;
