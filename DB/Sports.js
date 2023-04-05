fd15f5716c522d0c67d41aa994424ca41fb75eea
kevin <kklein166@gmail.com>
2023-04-05 15:08:27 -0500

    added put requests for toggle switch in Event, eventCount not incrementing/decrementing correctly

--- DB/Sports.js
+++ DB/Sports.js
@@ -16,50 +16,51 @@ const sportSchema = new Schema({
 const Sports = mongoose.model('Sports', sportSchema);
 
 // Sport Model Seed
-const sport = [{
-  category: 'Volleyball',
-  image: 'client/images/volleyball_icon.png',
-},
-{
-  category: 'Basketball',
-  image: 'client/images/basketball_icon.png',
-},
-{
-  category: 'Tennis',
-  image: 'client/images/tennis_icon.png',
-},
-{
-  category: 'Football',
-  image: 'client/images/football_icon.png',
-},
-{
-  category: 'Soccer',
-  image: 'client/images/soccer_icon.png',
-},
-{
-  category: 'Ultimate frisbee',
-  image: 'client/images/frisbee_icon.png',
-},
-{
-  category: 'Softball',
-  image: 'client/images/softball_icon.png',
-},
-{
-  category: 'Bowling',
-  image: 'client/images/bowling_icon.png',
-},
-{
-  category: 'Rugby',
-  image: 'client/images/rugby_icon.png',
-},
-{
-  category: 'Ping Pong',
-  image: 'client/images/pingpong_icon.png',
-}];
+const sport = [
+  {
+    category: 'Volleyball',
+    image: 'client/images/volleyball_icon.png',
+  },
+  {
+    category: 'Basketball',
+    image: 'client/images/basketball_icon.png',
+  },
+  {
+    category: 'Tennis',
+    image: 'client/images/tennis_icon.png',
+  },
+  {
+    category: 'Football',
+    image: 'client/images/football_icon.png',
+  },
+  {
+    category: 'Soccer',
+    image: 'client/images/soccer_icon.png',
+  },
+  {
+    category: 'Ultimate frisbee',
+    image: 'client/images/frisbee_icon.png',
+  },
+  {
+    category: 'Softball',
+    image: 'client/images/softball_icon.png',
+  },
+  {
+    category: 'Bowling',
+    image: 'client/images/bowling_icon.png',
+  },
+  {
+    category: 'Rugby',
+    image: 'client/images/rugby_icon.png',
+  },
+  {
+    category: 'Ping Pong',
+    image: 'client/images/pingpong_icon.png',
+  },
+];
 
-Sports.insertMany(sport)
-  .catch((err) => {
-    console.log('no duplicates allowed', err);
-  });
+Sports.insertMany(sport).catch((err) => {
+  console.log('no duplicates allowed', err);
+});
 
 module.exports = mongoose.model('Sports', sportSchema);
--- server/passport.js
+++ server/passport.js
@@ -8,24 +8,30 @@ const { Users } = require('../DB/models');
 
 passport.use(Users.createStrategy());
 
-passport.use(new GoogleStrategy(
-  {
-    clientID: ENV.CLIENT_ID,
-    clientSecret: ENV.CLIENT_SECRET,
-    callbackURL: '/auth/google/callback',
-    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
-  },
-  ((accessToken, refreshToken, profile, cb) => {
-    // eslint-disable-next-line max-len
-    Users.findOrCreate({
-      email: profile.emails[0].value,
-      firstName: profile.name.givenName,
-      lastName: profile.name.familyName,
-      googleIdNumber: profile.id,
-      image: profile.photos[0].value,
-    }, (err, user) => cb(err, user));
-  }),
-));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: ENV.CLIENT_ID,
+      clientSecret: ENV.CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+      userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      // eslint-disable-next-line max-len
+      Users.findOrCreate(
+        {
+          email: profile.emails[0].value,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          googleIdNumber: profile.id,
+          image: profile.photos[0].value,
+          eventCount: 0,
+        },
+        (err, user) => cb(err, user)
+      );
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
