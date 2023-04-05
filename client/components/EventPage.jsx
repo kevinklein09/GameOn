37c62dad2e9d7b40dc1be322f72d456ab8aea7b7
matthewpmi <mpmekita.gmail.com>
2023-04-05 12:27:58 -0400

    ADD input box/button, axios post

--- client/components/EventPage.jsx
+++ client/components/EventPage.jsx
@@ -8,7 +8,7 @@ function EventPage() {
   const context = useContext(UserContext);
   // use the eventId to fetch data for the specific event
   const [event, setEvent] = useState({});
-
+  const [messages, setMessages] = useState([]);
   useEffect(() => {
     getEvent();
   }, []);
@@ -18,16 +18,53 @@ function EventPage() {
       .then((event) => {
         console.log(event);
         setEvent(event.data);
+        setMessages(event.data.messages);
       })
       .catch((err) => {
         console.error('failed to get event:', err);
       });
   };
 
+  const postMessage = (message) => {
+    axios.post(
+      `/event/${eventId}/message`,
+      {
+        username: 'todd',
+        message: 'anything',
+      },
+    )
+      .then(() => {
+        getEvent();
+      })
+      .catch((err) => {
+        console.error('could not post message:', err);
+      });
+  };
+
   if (context) {
     return (
-      <div>
-        {event?.locName}
+      <div className="bulletin">
+        <h2>
+          {`${event?.catName} at ${event?.locName}`}
+        </h2>
+        <h6>
+          {`${new Date(event?.date).toLocaleDateString()} at ${event?.time}`}
+        </h6>
+        <div className="messages">
+          {messages.map((message) => (
+            <div key={message.id}>
+              <p>Playa: {message.username}</p>
+              <p>{message.message}</p>
+            </div>
+          ))}
+        </div>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="Type your message here"
+          />
+          <button onClick={postMessage}>Send</button>
+        </div>
       </div>
     );
   }
