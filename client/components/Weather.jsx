eb5640e21d269c3e2f92d2a4fd000ec571d96b6b
itsbrotherdan <jordandavis919@gmail.com>
2023-04-04 19:43:53 -0500

    added pesudo code and weather icons

--- client/components/Weather.jsx
+++ client/components/Weather.jsx
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from 'react';import axios from 'axios';
+import React, { useState, useEffect } from 'react'; import axios from 'axios';
 
-//import icons for weather forecast
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun, faCloud, faCloudRain, faSnowflake } from "@fortawesome/free-solid-svg-icons";
+// import icons for weather forecast
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSun, faCloud, faCloudRain, faSnowflake,
+} from '@fortawesome/free-solid-svg-icons';
 
 const API_URL = 'https://api.open-meteo.com/v1/forecast/daily';
 
 const Weather = () => {
+  // giving state
   const [weatherData, setWeatherData] = useState(null);
 
+  // potential useEffect
   useEffect(() => {
     axios.get(API_URL)
       .then((res) => {
@@ -19,10 +23,37 @@ const Weather = () => {
       });
   }, []);
 
+  // loading screen display
   if (!weatherData) {
     return <div>Loading weather data...</div>;
   }
 
-  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  // days of the week to display
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+  // icons for types of weather
+  const getWeatherIcon = (weatherCondition) => {
+    switch (weatherCondition) {
+      case 'clear':
+        return <FontAwesomeIcon icon={faSun} />;
+      case 'partly-cloudy':
+        return <FontAwesomeIcon icon={faCloud} />;
+      case 'cloudy':
+        return <FontAwesomeIcon icon={faCloud} />;
+      case 'light-rain':
+        return <FontAwesomeIcon icon={faCloudRain} />;
+      case 'heavy-rain':
+        return <FontAwesomeIcon icon={faCloudRain} />;
+      case 'snow':
+        return <FontAwesomeIcon icon={faSnowflake} />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="weather-forecast">
+      <h2>Weekly Weather Forecast</h2>
+    </div>
+  );
 };
