57a24ac17253b58d82890d5c0789df3d5027736b
opsparkbtc <brendantcarmichael@gmail.com>
2023-04-06 13:45:55 -0500

    Frontend and Routing

--- server/index.js
+++ server/index.js
@@ -290,6 +290,34 @@ app.post('/event/:eventId/message', (req, res) => {
   );
 });
 
+// Team Routes
+
+// Retrieve teams from database - TeamList.jsx
+app.get('/api/get', (req, res) => {
+  TeamList.findOne({ _id: req.query.id })
+    .then((teams) => res.status(200).send(teams))
+    .catch((error) => res.sendStatus(500));
+});
+
+// Add a team - CreateTeam.jsx
+app.post('/api/teamList', (req, res) => {
+  const {
+    owner,
+    teamName,
+    playerList
+  } = req.body;
+
+  TeamList.create({
+    owner,
+    teamName,
+    playerList
+  })
+    .then((team) => res.status(200).send(team))
+    .catch((error) => res.sendStatus(500));
+});
+
+
+
 app.listen(port, () => {
   console.log(`
   Listening at: localhost:${port}
